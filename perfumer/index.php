be8b5f276849019aeaff9fdd699f74c976439920
dianagabr <focadianagabriela@yahoo.com>
2019-05-22 11:18:46 +0300

    Create index.php

---
+++ perfumer/index.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * PIP v0.5.3
+ */
+//Start the Session
+session_start(); 
+// Defines
+define('ROOT_DIR', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
+define('APP_DIR', ROOT_DIR .'app' . DIRECTORY_SEPARATOR);
+// Includes
+require(APP_DIR .'conf'. DIRECTORY_SEPARATOR . 'config.php');
+require(ROOT_DIR .'core'. DIRECTORY_SEPARATOR . 'model.php');
+require(ROOT_DIR .'core'. DIRECTORY_SEPARATOR . 'view.php');
+require(ROOT_DIR .'core'. DIRECTORY_SEPARATOR . 'controller.php');
+require(ROOT_DIR .'core'. DIRECTORY_SEPARATOR . 'route.php');
+// Define base URL
+global $config;
+define('BASE_URL', $config['base_url']);
+route();
+?>
