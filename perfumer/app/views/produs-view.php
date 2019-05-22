22b0d0135151be2e7be332adef17044deab6f5cb
dianagabr <focadianagabriela@yahoo.com>
2019-05-22 22:59:27 +0300

    Update produs-view.php

--- perfumer/app/views/produs-view.php
+++ perfumer/app/views/produs-view.php
@@ -1,63 +1,91 @@
 <?php include 'header.php'; ?>
 <section>
 <div class=produs>
-    <img src="<?php echo BASE_URL; ?>assets/images/prada.png" style="width:23%; height:23%;">
-
+    <img src="<?php echo BASE_URL . $produs["imagine"]?>" style="width:23%; height:23%;">
     <div class="all">
-    <div class="titlu-produs">
-      Prada Candy ApÃ„Æ’ de toaletÃ„Æ’
+      <div class="titlu-produs">
+       <?php echo (strtoupper($produs["nume"]). ' ' . strtoupper($produs["categorie"])); ?>
       </div>
     <div class="detalii-produs">
-			<div class="rowprodus">
-  <div class="informatii">
-           <p> PreÃˆâ€º </p>
-            <p> Disponibilitate</p>
-            <p> Cantitate </p>
-            </div>
-            <div class="info-show">
-            <p> 255 LEI </p>
-            <p class="stoc" style="color:green;"> ÃƒÅ½n stoc </p>
-            <form>
-              <label for="variante"></label>
-              <select name="variante" id="variante">
-           <option value="30 ml">30 ml</option>
-           <option value="50 ml">50 ml</option>
-           <option value="100 ml">100 ml</option>
-       </select>
-   </form>
-              </div>
-						</div>
+	  	<table style="width:100%">
+        <tr>
+          <th>Cantitate</th>
+          <th>Disponibilitate</th> 
+          <th>Pret</th>
+        </tr>
+        <tr>
+          <td> 
+					  <form method="post" action="#">
+					    <select name="variante" id="variante">
+								<option value=""></option>
+                <option value="30 ml">30 ml</option>
+                <option value="50 ml">50 ml</option>
+                <option value="100 ml">100 ml</option>
+						  </select>
+            </form>
+          </td>              
+						<?php 
+						   if(isset($_POST["variante"])){
+									$varianta=$_POST["variante"];
+									echo $varianta;
+									switch($variante) {
+                      case '30 ml' : if($produs["stoc_30"]>0) {
+												echo '<td> <style="color:green"> In stoc </td>';
+											}
+											else{
+												echo '<td> <style="color:red"> Stoc insuficient </td>';
+											}
+											echo '<td>' . $produs["pret_30"] . '</td>';  
+											break;
+											case '50 ml' : if($produs["stoc_50"]>0) {
+												echo '<td> <style="color:green"> In stoc </td>';
+											}
+											else{
+												echo '<td> <style="color:red"> Stoc insuficient </td>';
+											}
+											echo '<td>' . $produs["pret_50"] . '</td>';  
+											break;
+											case '30 ml' : if($produs["stoc_100"]>0) {
+												echo '<td> <style="color:green"> In stoc </td>';
+											}
+											else{
+												echo '<td> <style="color:red"> Stoc insuficient </td>';
+											}
+											echo '<td>' . $produs["pret_100"] . '</td>';  
+											break;
+									}
+									
+							 }
+						?>
+        </tr>
+      </table>
     </div>
 		<div class="buttons">
-		<button><i class="far fa-heart"></i>AdaugÃ„Æ’ ÃƒÂ®n wishlist</button>
-		<button><i class="fa fa-shopping-cart"></i>AdaugÃ„Æ’ ÃƒÂ®n coÃˆâ„¢</button>
+		<button><i class="far fa-heart"></i> Adauga in wishlist</button>
+		<button><i class="fa fa-shopping-cart"></i> Adauga in cos</button>
 	</div>
   </div>
 
 	</div>
 	<div class="detalii-produs2">
 	<div class="compozitie">
-		<h4>CompoziÃˆâ€ºie</h4>
+		<h4>Compozitie</h4>
 		  <div class="note">
-				<h6><b>Note de bazÃ„Æ’</b> :caramel</h6>
+				<h6><b>Note de baza: </b> <?php echo $produs["note_baza"] ?> </h6>
 			</div>
 			<div class="note">
-			 <h6><b>Note de inimÃ„Æ’</b> :benzoin</h6>
+			 <h6><b>Note de inima: </b> <?php echo $produs["note_inima"] ?> </h6>
 		 </div>
 		 <div class="note">
-			 <h6><b>Note de vÃƒÂ¢rf</b> :mosc</h6>
+			 <h6><b>Note de varf: </b> <?php echo $produs["note_varf"] ?> </h6>
 		 </div>
 		 <div class="note">
-			 <h6>Parfum oriental</h6>
+			 <h6> <?php echo 'Parfum ' . $produs["esenta"] ; ?></h6>
 		 </div>
 			</div>
 			<div class="descriere">
 				<h4>Descriere</h4>
-				Prada Candy este un parfum floral ce se potriveste tuturor femeilor carora nu le este teama sa-si exprima propriile opinii. Preda-te tentatiei in forma unei puteri roz adusa de parfumul Prada Candy. Iesi din anonimat si straluceste. Incearca sa faci lucrurile pe cont propriu si primeste energia necesara pentru a invinge obstacolele de zi cu zi.
-
-Notele de varf usor florale fac pe oricine sa se indragosteasca instantaneu de acest parfum. Notele centrale contin esente senzuale de mosc alb, ce-ti confera incredere si determinare sa devi o femeie increzatoare si stralucitoare. Incantatoarele acorduri seducatoare de caramel iti incanta simturile in profunzime si scoate la suprafata adevarata personalitate!
-
-Una dintre cele mai importante case de moda, Prada a inceput sa comercializeze parfumuri acum mult timp. Indragosteste-te de calitatea si caracterul acestor parfumuri pline de flori, fructe si condimente atent selectionate. Este apreciat in special de catre cei ce iubesc stilul si luxul conferit de aceasta marca prestigioasa, prezentate in sticluta cu forme extrem de moderne. Prada Candy este cu siguranta pe placul tau. Indrazneste sa-ti exprimi opinia. Doar femeile increzatoare in fortele lor proprii, care stiu exact ceea ce vor, pot obtine orice isi doresc!
+				<?php echo $produs["descriere"]; ?>
 		</div>
 		</div>
   </section>
