42b4bd095aff2f619c263ed1c485aba81c6cfe72
dianagabr <focadianagabriela@yahoo.com>
2019-05-22 11:25:42 +0300

    Create index-view.php

---
+++ app/views/index-view.php
@@ -0,0 +1,116 @@
+<?php include 'header.php' ?>
+
+<section>
+
+		<div id="reducere">
+			<div id="reducereLeft">
+				<div id="reducereText">
+					10% REDUCERE
+				</div>
+				<a href="#">
+					<div id="reducereText2">
+						DescoperÄƒ oferta
+					</div>
+				</a>
+			</div>
+		</div>
+
+
+		<div id="topNoutati">
+			<div id="titleTOP">
+				<h2 style="font-family: Bitter "><b>TOP NOUTÄ‚TI</b></h2>
+
+			</div>
+
+			<div id="topNoutatiFoto">
+				<div class="fotoNoutati">
+					<a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/Miss-Dior.png" > </a>
+					<div class="textTop">
+						Dior <b>Miss Dior Blooming Bouquet</b>
+					</div>
+				</div>
+				<div class="fotoNoutati">
+					<a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/Chanel-parfum.jpg"> </a>
+					<div class="textTop">
+						Chanel <b>Coco Mademoiselle</b>
+					</div>
+				</div>
+				<div class="fotoNoutati">
+					<a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/dolce3.png"> </a>
+					<div class="textTop">
+						Dolce & Gabbana  <b>The One</b>
+					</div>
+				</div>
+				<div class="fotoNoutati">
+					<a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/invictus.png" > </a>
+					<div class="textTop">
+						Paco Rabanne <b>Invictus</b>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="bestSeller">
+			<div id="bestSellerW">
+				<div class="bestSellerText">
+					BESTSELLER FEMEI
+				</div>
+				<a href="#">
+					<div class="bestSellerText2">
+						DescoperÄƒ oferta
+					</div>
+				</a>
+
+			</div>
+			<div id="bestSellerM">
+				<div class="bestSellerText">
+					BESTSELLER BÄ‚RBAÈšI
+				</div>
+				<a href="#">
+					<div class="bestSellerText2">
+						DescoperÄƒ oferta
+					</div>
+				</a>
+
+			</div>
+		</div>
+
+
+		<div id="topBranduri">
+			<div id="titleTopBrand">
+				<h2 style="font-family: Bitter "><b>TOP BRANDURI</b></h2>
+			</div>
+
+			<div id="topBranduriFoto">
+				<div class="foto">
+					<img alt="dior" src="<?php echo BASE_URL; ?>assets/images/Dior_Logo.png"  width="210">
+				</div>
+				<div class="foto">
+					<img alt="chanel" src="<?php echo BASE_URL; ?>assets/images/Chanel_Logo.png" width="170">
+				</div>
+				<div class="foto">
+					<img alt="DG" src="<?php echo BASE_URL; ?>assets/images/Dolce-Gabbana-Logo.png"  width="200">
+				</div>
+				<div class="foto">
+					<img alt="paco" src="<?php echo BASE_URL; ?>assets/images/paco-rabanne-logo1.png"  width="200">
+				</div>
+
+				<div class="foto">
+					<img alt="hugo" src="<?php echo BASE_URL; ?>assets/images/Hugo-Boss-Logo.png"  width="200">
+
+				</div>
+				<div class="foto">
+					<img alt="ck" src="<?php echo BASE_URL; ?>assets/images/calvin-klein-logo.png"  width="200">
+				</div>
+				<div class="foto">
+					<img alt="hermes" src="<?php echo BASE_URL; ?>assets/images/logo-hermes.png"  width="200">
+				</div>
+
+			</div>
+		</div>
+
+
+</section>
+
+
+<?php  include 'footer.php'   ?>
