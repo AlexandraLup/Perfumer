52b94c7e9bcdef8901c66aa4e7477e5349747260
dianagabr <focadianagabriela@yahoo.com>
2019-05-22 11:25:57 +0300

    Create header.php

---
+++ app/views/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="RO">
+
+<head>
+	 <meta charset="UTF-8">
+	<title>Perfumer</title>
+	<link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/styles.css"   type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+
+</head>
+
+
+<body>
+
+	<header>
+		<div id="Div" >
+			<div id="Div1">
+				<form id="search-button" >
+				  	<input type="search" placeholder="CautÄƒ..">
+				</form>
+			</div>
+
+			<div id="Div2">
+				<div>
+					<h1 class="h1"><a href="index.html">Perfumer</a></h1>
+				</div>
+			</div>
+
+		</div>
+
+	</header>
+
+	<nav>
+		<ul>
+			<li class="FemeiBarbati"><a href="/parfumuriFemei">PARFUMURI FEMEI</a></li>
+			<li class="FemeiBarbati"><a href="/parfumuriFemei">PARFUMURI BÄ‚RBAÈšI</a></li>
+			<li class="FemeiBarbati"><a href="/parfumuriFemei">NOUTÄ‚ÈšI</a></li>
+		</ul>
+
+		<ul>
+			<li class="ContCos"><a href="login.html">CONT <i class="fa fa-user-circle" ></i></a></li>
+			<li class="ContCos"><a href="shoppingcart.html">COÈ˜ <i class="fa fa-shopping-cart"></i></a></li>
+			<li class="ContCos"><a href="myaccount.html">WISHLIST <i class="fa fa-heart"></i></a></li>
+		</ul>
+	</nav>
