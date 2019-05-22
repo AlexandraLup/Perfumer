6713791453cfc3f9e1da5e5f9017b642ae416457
dianagabr <focadianagabriela@yahoo.com>
2019-05-22 11:28:00 +0300

    Create model.php

---
+++ core/model.php
@@ -0,0 +1,54 @@
+<?php
+class Model {
+	private $connection;
+	public function __construct()
+	{
+		global $config;
+		
+		$this->connection = new mysqli($config['db_host'], $config['db_username'], $config['db_password'],$config['db_name']) or die('MySQL Error: '. mysql_error());
+	}
+	public function escapeString($string)
+	{
+		return $this->connection->real_escape_string($string);
+	}
+	public function escapeArray($array)
+	{
+	    array_walk_recursive($array, create_function('&$v', '$v = mysql_real_escape_string($v);'));
+		return $array;
+	}
+	
+	public function to_bool($val)
+	{
+	    return !!$val;
+	}
+	
+	public function to_date($val)
+	{
+	    return date('Y-m-d', $val);
+	}
+	
+	public function to_time($val)
+	{
+	    return date('H:i:s', $val);
+	}
+	
+	public function to_datetime($val)
+	{
+	    return date('Y-m-d H:i:s', $val);
+	}
+	
+	public function query($qry)
+	{
+		$result = mysqli_query($this->connection,$qry) or die('MySQL Error: '. mysql_error());
+		$resultObjects = array();
+		while($row = mysqli_fetch_array($result)) $resultObjects[] = $row;
+		return $resultObjects;
+	}
+	public function execute($qry)
+	{
+		$exec = mysql_query($qry) or die('MySQL Error: '. mysql_error());
+		return $exec;
+	}
+    
+}
+?>
