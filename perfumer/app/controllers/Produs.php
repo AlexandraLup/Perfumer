a4cf065428cce611b136372e2417de0aa3f76573
dianagabr <focadianagabriela@yahoo.com>
2019-05-22 22:59:14 +0300

    Update Produs.php

--- perfumer/app/controllers/Produs.php
+++ perfumer/app/controllers/Produs.php
@@ -1,10 +1,13 @@
 <?php
+include APP_DIR.'models/ProdusModel.php';
 class Produs extends Controller {
 	
 	function index()
 	{
-	   //	$model=new Login_model();
+		$model=new  ProdusModel();
 		$template = $this->loadView('produs-view');
+		$info = $model->getDetails('candy prada');
+		$template->set('produs', $info) ;
 		$template->render();
 	}
 }
