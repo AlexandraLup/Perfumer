82cab7cdcd10108da56ef6904413c5c311f9d00e
dianagabr <focadianagabriela@yahoo.com>
2019-05-22 11:37:06 +0300

    update config.php

--- perfumer/app/conf/config.php
+++ perfumer/app/conf/config.php
@@ -1,5 +1,5 @@
 <?php 
-$config['base_url'] = 'http://localhost/perfumer/'; 
+$config['base_url'] = 'http://localhost/Perfumer/perfumer/'; 
 $config['default_controller'] = 'index'; // Default controller to load
 $config['error_controller'] = 'Eroare'; // Controller used for errors (e.g. 404, 500 etc)
 $config['db_host'] = 'localhost'; // Database host (e.g. localhost)
