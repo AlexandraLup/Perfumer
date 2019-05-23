c6d48fc0bd3ff23bc9f568a21e54810fe62f9e2a
dianagabr <focadianagabriela@yahoo.com>
2019-05-23 21:22:04 +0300

    Create admin-index.php

---
+++ perfumer/app/views/admin-index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="RO">
+
+<head>
+	 <meta charset="UTF-8">
+	<title>Perfumer</title>
+	<link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/styles.css"   type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+</head>
+
+
+<body>
+	<header>
+	    <div id="Div2">
+		    <div>
+				<h1 class="h1" style="padding-left:90%">Perfumer</a></h1>
+			</div>
+		</div>
+    </header>
+
+    <section>
+        
+    <h2 class="admin-titlu">AUTENTIFICA-TE</h2>
+
+<form action="" method="POST">
+
+  <div class="login-container">
+    <label for="email"><b>Username</b></label>
+    <input type="text" placeholder="Introdu adresa de email" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Introdu parola" name="psw" required>
+        
+    <button type="submit">Login</button>
+  </div>
+</form>
+
+</section>
+
+
+    </body>
+</html>
