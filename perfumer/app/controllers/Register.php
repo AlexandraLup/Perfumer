eca79580b7fdcc5b14931f6c1631799e8bea8dc7
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-05-23 23:32:19 +0200

    Create Register.php

---
+++ perfumer/app/controllers/Register.php
@@ -0,0 +1,46 @@
+<?php
+include APP_DIR . 'models/RegisterModel.php';
+include APP_DIR.'helpers/session_helper.php';
+class Register extends Controller {
+	
+	function index()
+	{
+		$model = new RegisterModel();
+		$template = $this->loadView('register-view');
+		
+
+		if(isset($_POST ["nume"]) && isset($_POST["prenume"])){
+
+			$nume= $_POST ["nume"];
+			$prenume = $_POST["prenume"];
+			$adresa = $_POST["adresa"];
+			$sex = $_POST["sex"];
+			$email= $_POST["email"];
+			$telefon= $_POST["telefon"];
+			$parola1= $_POST["pass1"];
+			$parola2= $_POST["pass2"];
+
+			$info= $model->getEmail($email);
+			if($info[0] == '0')
+			{//email-ul e valid.
+				
+				if($parola1==$parola2)
+				{
+					$parola1=password_hash($parola2, PASSWORD_DEFAULT);
+					$model->createAccount($nume, $prenume,$adresa, $sex, $email, $telefon, $parola1);
+
+					$this->redirect('login');
+				}
+				else
+					$template->set('parolaNeconfirmata', 0);
+			}
+			else
+				$template->set('emailNevalid',0);
+		
+		}
+
+
+		$template->render();
+	}
+}
+?>
