0311ea6b26ffdb30ea0bdc5063b4326dc9f72edb
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-05-23 23:32:42 +0200

    Update login-view.php

--- perfumer/app/views/login-view.php
+++ perfumer/app/views/login-view.php
@@ -3,7 +3,7 @@
 <div class="main">
 
   <div class="title">
-    <h2> Autentifica-te sau creeaza cont </h2>
+    <h2> Autentifică-te sau creează cont </h2>
   </div>
 
   <div class="row1">
@@ -11,13 +11,21 @@
       <h4> SUNT DEJA CLIENT </h4>
       <form class="login" action="" method="POST">
         <label for="email"><b>Email</b></label>
-        <input type="text" placeholder="Introdu adresa de email" id="email" name="email" required>
-        <label for="psw"><b>Parola</b></label>
-        <input type="password" placeholder="Introdu parola"  id="psw" name="psw" required>
+        <input type="text" placeholder="Enter E-mail" id="email" name="email" required>
+        <label for="psw"><b>Parolă</b></label>
+         <?php 
+            if(isset($parolaGresita))
+              echo '<p style="color:red;"> Ați greșit parola! </p>'
+        ?>
+        <input type="password" placeholder="Enter Password"  id="psw" name="psw" required>
         <button type="submit">Login</button>
         <span class="psw"> <a href="#">Ti-ai uitat parola?</a></span>
         <br>
-        <label>
+        <?php 
+            if(isset($login))
+              echo '<p style="color:red;"> Înregistrează-te mai întâi! </p>'
+        ?>
+        <label> 
           <input type="checkbox" checked="checked" name="remember"> Remember me
         </label>
       </form>
@@ -28,13 +36,13 @@
       <div class="box">
         <h5> Benefiicile clientului </h5>
         <p> Istoricul comenzilor</p>
-        <p> Vezi lista de preferinte</p>
-        <p> PrimeÃˆâ„¢ti prin poÃˆâ„¢tÃ„Æ’ ultimele noutÃ„Æ’Ãˆâ€ºi despre tendinÃˆâ€ºe Ãˆâ„¢i </p>
-        <p> promoÃˆâ€ºii</p>
+        <p> Vezi lista de preferințe</p>
+        <p> Primești prin poștă ultimele noutăți despre tendințe și </p>
+        <p> promoții</p>
         <br>
       </div >
       <div class="box1" >
-        <button onclick="window.location.href = 'inregistrare.html';"> ÃƒÅ½nregistreazÃ„Æ’-te </button>
+        <button onclick="window.location.href = 'register';"> Înregistrează-te </button>
       </div>
     </div>
 
