2ca2a4f22ae9c0918c1efa279496c5774b540b32
dianagabr <focadianagabriela@yahoo.com>
2019-05-23 13:23:18 +0300

    Update Login.php

--- perfumer/app/controllers/Login.php
+++ perfumer/app/controllers/Login.php
@@ -1,12 +1,22 @@
 
 <?php
+include APP_DIR . 'models/LoginModel.php';
 include APP_DIR.'helpers/session_helper.php';
 class Login extends Controller {
 	
 	function index()
 	{
-		//$model=$this->loadModel('LoginModel');
+		$model = new LoginModel();
 		$template = $this->loadView('login-view');
+		
+
+		if(isset($_POST["email"])){
+			$email= $_POST["email"];
+			$info =$model->getEmail($email);
+			
+			if($info[0] == '0')
+				var_dump($info[0]);
+		}
 		$template->render();
 	}
 }
