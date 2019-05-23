3e7cf05dacd479a236b5e1123408df74cb5ca443
dianagabr <focadianagabriela@yahoo.com>
2019-05-23 13:23:31 +0300

    Update login-view.php

--- perfumer/app/views/login-view.php
+++ perfumer/app/views/login-view.php
@@ -3,19 +3,19 @@
 <div class="main">
 
   <div class="title">
-    <h2> AutentificÃ„Æ’-te sau creeazÃ„Æ’ cont </h2>
+    <h2> Autentifica-te sau creeaza cont </h2>
   </div>
 
   <div class="row1">
     <div class="column-left">
       <h4> SUNT DEJA CLIENT </h4>
-      <form class="login" action="index.html">
+      <form class="login" action="" method="POST">
         <label for="email"><b>Email</b></label>
-        <input type="text" placeholder="Enter E-mail" id="email" required>
+        <input type="text" placeholder="Enter E-mail" id="email" name="email" required>
         <label for="psw"><b>ParolÃ„Æ’</b></label>
-        <input type="password" placeholder="Enter Password"  id="psw" required>
+        <input type="password" placeholder="Enter Password"  id="psw" name="psw" required>
         <button type="submit">Login</button>
-        <span class="psw"> <a href="#">ÃˆÅ¡i-ai uitat parola?</a></span>
+        <span class="psw"> <a href="#">Ti-ai uitat parola?</a></span>
         <br>
         <label>
           <input type="checkbox" checked="checked" name="remember"> Remember me
