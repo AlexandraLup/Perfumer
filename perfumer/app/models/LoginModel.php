49de3717260233a6fef1e1fb7f54fbe88fc8619d
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-05-24 11:05:08 +0200

    Update LoginModel.php

--- perfumer/app/models/LoginModel.php
+++ perfumer/app/models/LoginModel.php
@@ -3,7 +3,7 @@ class LoginModel extends Model {
 	
 	public function getEmail($email)
 	{
-		$query= 'select count(email) from utilizatori where email="'. $email .'"';
+		$query= 'select email from utilizatori where email="'. $email .'"';
 		$result= $this->query($query);
 		return $result;
 	}
