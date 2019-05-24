ed3be25aa0e645d9d48e6a98879b13d4c3d9bd4d
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-05-24 11:26:34 +0200

    Update admin-rapoarte.php

--- perfumer/app/views/admin-rapoarte.php
+++ perfumer/app/views/admin-rapoarte.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-   
+
 </head>
 <body>
 	<header>
@@ -25,7 +25,7 @@
   <a href="#"><i class="fa fa-fw fa-envelope"></i>Sterge produs</a> 
 </div>
 <section>
-      
+
 </section>
 </body>
-</html>
+</html> 
