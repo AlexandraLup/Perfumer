8cb71b929fd627ddb7fe5703f880dc89f13684fe
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-05-27 10:53:27 +0200

    Create Admin.php

---
+++ app/controllers/Admin.php
@@ -0,0 +1,42 @@
+<?php
+class Admin extends Controller {
+	
+	function index()
+	{   
+		$template = $this->loadView('admin-index');
+		$template->render();
+	}
+
+	function rapoarte(){
+   
+		$template = $this->loadView('admin-rapoarte');
+		$template->render();
+	}
+
+	function add(){
+
+		$template = $this->loadView('admin-add');
+		/*if (isset($_POST)) {
+			$barcode = trim($_POST['barcode']);
+			$name    = trim($_POST['name']);
+			$gender    = trim($_POST['gender']);
+			$price1   = (float) $_POST['price1'];
+			$qty1     = (int) $_POST['qty1'];
+			$price2   = (float) $_POST['price2'];
+			$qty2     = (int) $_POST['qty2'];
+			$qty3     = (int) $_POST['qty3'];
+            $price3   = (float) $_POST['price3'];
+			$image   = trim($_POST['image']);
+			$essense   = trim($_POST['essence']);
+			$category  = trim($_POST['category']);
+			$base  = trim($_POST['base']);
+			$heart   = trim($_POST['heart']);
+			$top   = trim($_POST['top']);
+			$description = trim($_POST['description']);
+		}
+		*/
+		$template->render();
+	}
+
+}
+?>
