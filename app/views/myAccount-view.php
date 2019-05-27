b406daf0e4177d213eed246442b2e2439f99d98c
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-05-27 10:55:01 +0200

    Create myAccount-view.php

---
+++ app/views/myAccount-view.php
@@ -0,0 +1,66 @@
+<?php 
+include "header.php";
+
+?>
+
+
+		  
+
+<section>
+<div class="menu">
+    <div class="lista">
+    <a href="myaccount.html"><i class="far fa-heart"></i> Produse favorite</a>
+    </div>
+    <div class="lista">
+    <a href="personaldata.html"><i class="far fa-clipboard"></i> Date personale</a>
+    </div>
+    <div class="lista">
+    <a href="orderhistory.html"><i class="fas fa-warehouse"></i> Istoricul comenzilor</a>
+    </div>
+</div>
+<div class="title2">
+<h3><b>Wishlist</b></h3>
+</div>
+
+<div id="wishlist">
+	<div class="items">
+		<img alt="#" class="item-photo" src="images/Miss-Dior.png" style="width:250px; height:250px;">
+		<div class="text-item">
+			Dior <b>Miss Dior Blooming Bouquet</b>
+		</div>
+		<div class="adauga">
+			<button>Adaugă în coș</button>
+		</div>
+	</div>
+	<div class="items">
+		<img alt="#" class="item-photo" src="images/Chanel-parfum.jpg" style="width:250px; height:250px;">
+		<div class="text-item">
+			Chanel <b>Coco Mademoiselle</b>
+		</div>
+		<div class="adauga">
+			<button>Adaugă în coș</button>
+		</div>
+	</div>
+	<div class="items">
+		<img alt="#" class="item-photo" src="images/dolce3.png" style="width:250px; height:250px;" >
+		<div class="text-item">
+			Dolce & Gabbana  <b>The One</b>
+		</div>
+		<div class="adauga">
+			<button>Adaugă în coș</button>
+		</div>
+	</div>
+	<div class="items">
+		<img alt="#" class="item-photo" src="images/invictus.png" style="width:250px; height:250px;">
+		<div class="text-item">
+			Paco Rabanne <b>Invictus</b>
+		</div>
+		<div class="adauga">
+			<button>Adaugă în coș</button>
+		</div>
+	</div>
+</div>
+
+</section>
+
+<?php  include 'footer.php'   ?>
