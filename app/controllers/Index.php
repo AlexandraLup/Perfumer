1b62989d8ab384d41ca34010c7c69fd0167d66e7
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-05-27 10:53:34 +0200

    Create Index.php

---
+++ app/controllers/Index.php
@@ -0,0 +1,18 @@
+<?php
+class Index extends Controller {
+	
+	function index()
+	{
+	   //	$model=new Login_model();
+		$template = $this->loadView('index-view');
+
+
+		if(isset($_POST["logout"])){
+			session_destroy();
+			$this->redirect('login');
+		}
+
+			$template->render();
+	}
+}
+?>
