60569a43ace2252c5ffaab32d4d40a460cca7570
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-05-28 20:34:29 +0200

    Update parfumurifemei-view.php

--- app/views/parfumurifemei-view.php
+++ app/views/parfumurifemei-view.php
@@ -5,10 +5,10 @@
 <div style="background-color: white; height: 1000px">
   <div class="columnPF parfumuriLeft">
       <div class="filtreParfum"> Filtru </div>
-      <div class="filtru"> <a href="crescator.html"> PreÃˆâ€º crescator </a></div>
-      <div class="filtru"> <a href="#"> PreÃˆâ€º descrescator </a></div>
+      <div class="filtru"> <a href="crescator.html"> Preț  crescator </a></div>
+      <div class="filtru"> <a href="#"> Preț descrescator </a></div>
       <div class="filtru"> <a href="#"> Cele mai noi </a></div>
-      <div class="filtru"> <a href="#"> Cele mai vÃƒÂ¢ndute </a></div>
+      <div class="filtru"> <a href="#"> Cele mai vândute </a></div>
 
   </div>
 
@@ -18,117 +18,43 @@
     <div class="titluPF"> Parfumuri femei</div>
     <div>  <h2> Bara pagini </h2></div>
         <div id="topNoutatiFoto">
-            <div class="fotoNoutati">
-                <a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/Miss-Dior.png" > </a>
-                <div class="textParfumuri">
-                  Dior <b>Blooming Bouquet</b>
-                </div>
-                <div class="textPret">
-                  320,00 RON
-                </div>
-            </div>
-            
-            <div class="fotoNoutati">
-                <a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/dolce3.png"> </a>
-                <div class="textParfumuri">
-                  Dolce & Gabbana  <b>The One</b>
-                </div>
-                <div class="textPret">
-                  250,00 RON
-                </div>
-            </div>
-            <div class="fotoNoutati">
-                <a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/gucci-guilty.jpg" > </a>
-                <div class="textParfumuri">
-                  Gucci <b>Guilty</b>
-                </div>
-                <div class="textPret">
-                  220,00 RON
-                </div>
-            </div>
-            <div class="fotoNoutati">
-                <a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/armani-sky.jpg" > </a>
-                <div class="textParfumuri">
-                  Giorgio Armani  <b>Sky di Gioia</b>
-                </div>
-                <div class="textPret">
-                  310,00 RON
-                </div>
-            </div>
-            <div class="fotoNoutati">
-                <a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/armani-intense.jpg" > </a>
-                <div class="textParfumuri">
-                  Giorgio Armani  <b> Armani Si Intense </b>
-                </div>
-                <div class="textPret">
-                  365,00 RON
-                </div>
-            </div>
-            <div class="fotoNoutati">
-                <a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/Chanel-parfum.jpg"> </a>
-                <div class="textParfumuri">
-                Chanel <b>Coco Mademoiselle</b>
-                </div>
-                <div class="textPret">
-                  220,00 RON
-                </div>
-            </div>
-            <div class="fotoNoutati">
-                <a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/CH-vip.jpg" > </a>
-                <div class="textParfumuri">
-                  Carolina Herrera   <b> 212 VIP </b>
-                </div>
-                <div class="textPret">
-                  179,00 RON
-                </div>
-            </div>
-            <div class="fotoNoutati">
-                <a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/valentino-va.png" > </a>
-                <div class="textParfumuri">
-                  Valentino   <b> Valentina </b>
-                </div>
-                <div class="textPret">
-                  279,00 RON
-                </div>
-            </div>
-            <div class="fotoNoutati">
-                <a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/thierry.jpg" > </a>
-                <div class="textParfumuri">
-                  Thierry Mugler <b> Alien Oud Majestueux </b>
-                </div>
-                <div class="textPret">
-                  405,00 RON
-                </div>
-            </div>
-            <div class="fotoNoutati">
-                <a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/bvlgari-pour.jpg" > </a>
-                <div class="textParfumuri">
-                  Bvlgari  <b> Pour Femme </b>
-                </div>
-                <div class="textPret">
-                  271,00 RON
-                </div>
-            </div>
-            <div class="fotoNoutati">
-                <a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/bvlgari-goldea.jpg" > </a>
-                <div class="textParfumuri">
-                  Bvlgari  <b> Goldea The Roman Night</b>
-                </div>
-                <div class="textPret">
-                  291,00 RON
-                </div>
-            </div>
-            <div class="fotoNoutati">
-                <a href="#"><img alt="#" class="dimensiuneFoto" src="<?php echo BASE_URL; ?>assets/images/balenciaga.jpg" > </a>
-                <div class="textParfumuri">
-                  Balenciaga  <b> Woman</b>
-                </div>
-                <div class="textPret">
-                  346,00 RON
-                </div>
-            </div>
 
+            
+
+    <?php  
+
+  $output = '';
+  if($total_row > 0)
+  { 
+    for ($i=0; $i<$total_row;$i++) 
+    { 
+      $output .= '
+      <div class="fotoNoutati">
+      <div class="col-sm-4 col-lg-3 col-md-3">
+        <div class="btnn-1">
+          <img src="'. $produs[$i]['imagine'] .'" alt="" class="img-responsive" >
+          <p align="center"><strong><a href="#">'. $produs[$i]['nume'] .'</a></strong></p>
+          <h4 style="text-align:center;" class="text-danger" >'. $produs[$i]['pret'] .' RON'.'</h4>
+           </div>
         </div>
+
+      </div>
+      ';
+    }
+  }
+  else
+  {
+    $output = '<h3>No Data Found</h3>';
+  }
+  echo $output;
+  
+    ?>
+
+            </div>
+          
+            
+
+       
  
   </div>
 </div>
