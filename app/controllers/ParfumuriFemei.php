a64ec6d03fb437f47c62989abea3dbd8affb4f8d
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-05-28 20:34:10 +0200

    Update ParfumuriFemei.php

--- app/controllers/ParfumuriFemei.php
+++ app/controllers/ParfumuriFemei.php
@@ -1,11 +1,22 @@
 <?php
+include APP_DIR . 'models/ParfumuriFModel.php';
+
 class ParfumuriFemei extends Controller {
 	
 	function index()
 	{
-	   //	$model=new Login_model();
+	    $model= new ParfumuriFModel();
 		$template = $this->loadView('parfumurifemei-view');
+
+		$info=$model->getDetails();
+		$info2=$model->getRowCount();
+		$count= (int)$info2[0];
+	
+		$template->set('total_row',$count);
+		$template->set('produs', $info);
+		
 		$template->render();
 	}
 }
 ?>
+
