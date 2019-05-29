a57975a9fb20a1d235c6547d3661a2c9484d22cb
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-05-29 10:21:33 +0200

    Update ParfumuriFModel.php

--- app/models/ParfumuriFModel.php
+++ app/models/ParfumuriFModel.php
@@ -12,6 +12,14 @@ class ParfumuriFModel extends Model {
 	public function getRowCount()
 	{   
         $query = "select count(nume) from produse where gen='femeie'";
+        if(isset($_POST["lowerSlider"], $_POST["upperSlider"]) && !empty($_POST["lowerSlider"]) && !empty($_POST["upperSlider"]))
+	{
+		$query .= "
+		 AND pret BETWEEN '".$_POST["lowerSlider"]."' AND '".$_POST["lowerSlider"]."'
+		";
+	}
+
+	echo $query;
 
   		$result= $this->query($query);
 	return $result;
