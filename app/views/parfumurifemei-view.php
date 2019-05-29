3e6d5a866dbb7d2c07355847c8ef23e78122015a
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-05-29 10:21:37 +0200

    Update parfumurifemei-view.php

--- app/views/parfumurifemei-view.php
+++ app/views/parfumurifemei-view.php
@@ -5,11 +5,36 @@
 <div style="background-color: white;  overflow:hidden;">
   <div class="columnPF parfumuriLeft">
       <div class="filtreParfum"> Filtru </div>
-      <div class="filtru"> <a href="crescator.html"> Preț  crescator </a></div>
+         <div class="filtru"> <a href="#"> Preț crescator </a></div>
       <div class="filtru"> <a href="#"> Preț descrescator </a></div>
       <div class="filtru"> <a href="#"> Cele mai noi </a></div>
       <div class="filtru"> <a href="#"> Cele mai vândute </a></div>
 
+         <div class="wrapper">
+    <fieldset class="filter-price">
+    
+    <div class="price-field">
+    <input type="range" min="0" max="5000" value="0" id="lower">
+    <input type="range" min="0" max="5000" value="5000" id="upper">
+    </div>
+  
+    <div class="price-wrap">
+      
+      <div class="price-container">
+        <div class="price-wrap-1">
+          <label for="one">$</label>
+          <input id="one">
+        </div>
+        <div class="price-wrap_line">-</div>
+        <div class="price-wrap-2">
+          <label for="two">$</label>
+          <input id="two">
+        </div>
+      </div>
+    </div>
+  
+      </fieldset>
+    </div>
   </div>
 
 
@@ -61,4 +86,60 @@
 
 
 </section>
+
+<script>
+$(document).ready(function(){
+
+    filter_data();
+
+    function filter_data()
+    {
+        $('.filter_data').html('<div id="loading" style="" ></div>');
+        var action = 'fetch_data';
+        var minimum_price = $('#lower').val();
+        var maximum_price = $('#upper').val();
+      
+        $.ajax({
+            url:"fetch_data.php",
+            method:"POST",
+            data:{action:action, minimum_price:minimum_price, maximum_price:maximum_price},
+            success:function(data){
+                $('.filter_data').html(data);
+            }
+        });
+    }
+
+    function get_filter(class_name)
+    {
+        var filter = [];
+        $('.'+class_name+':checked').each(function(){
+            filter.push($(this).val());
+        });
+        return filter;
+    }
+
+    $('.common_selector').click(function(){
+        filter_data();
+    });
+
+    $('#price_range').slider({
+        range:true,
+        min:1,
+        max:5000,
+        values:[1, 5000],
+        step:500,
+        stop:function(event, ui)
+        {
+            $('#price_show').html(ui.values[0] + ' - ' + ui.values[1]);
+            $('#hidden_minimum_price').val(ui.values[0]);
+            $('#hidden_maximum_price').val(ui.values[1]);
+            filter_data();
+        }
+    });
+
+});
+</script>
+
+
+
 <?php include 'footer.php'; ?>
