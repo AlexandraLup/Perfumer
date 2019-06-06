42f3b20f13dfc69d1f86c8f8c68b0d378f458589
dianagabr <focadianagabriela@yahoo.com>
2019-06-07 00:38:33 +0300

    Update produs-view.php

--- app/views/produs-view.php
+++ app/views/produs-view.php
@@ -1,11 +1,90 @@
-<?php include 'header.php'; ?>
+
+<!DOCTYPE html>
+<html lang="RO">
+
+<head>
+	 <meta charset="UTF-8">
+	<title>Perfumer</title>
+	<link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/styles.css"    type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">  </script>
+	<script>
+     $(document).ready(function(){
+         var commentCount = 2;
+			 $(".commbtn").click(function(){
+           $("#comentariu").load("load-comments.php");
+			 });
+
+		 });
+	</script>
+</head>
+
+
+<body>
+
+	<header>
+		<div id="Div" >
+			<div id="Div1">
+				<form id="search-button" method="post" action="#">
+					<input type="search" name="search" id="search" placeholder="Caută..">
+				</form>
+				<div id="back_result"> 
+                </div>  
+			</div>
+
+			<div id="Div2">
+				<div>
+					<h1 class="h1"><a href="index.php">Perfumer</a></h1>
+				</div>
+			</div>
+
+			<div id="Div3">
+				<div>
+				<?php 
+					if(isset($_SESSION["email"]))
+
+						echo '<p style="color: white;"> Bună, ' . $_SESSION['email'] .
+					" <form method='post'  >
+					<input type='submit' name='logout' value='logout'>  
+					</form> </p>
+					";
+					else
+						echo '<p style="color: white;"> Încă nu ești logat!</p>';
+				 ?> 
+
+				</div>
+			</div>
+
+		</div>
+
+	</header>
+
+	<nav>
+		<ul>
+			<li class="FemeiBarbati"><a href='parfumurifemei'>PARFUMURI FEMEI</a></li>
+			<li class="FemeiBarbati"><a href='parfumurifemei'>PARFUMURI BĂRBAȚI</a></li>
+			<li class="FemeiBarbati"><a href="parfumurifemei">NOUTĂȚI</a></li>
+		</ul>
+
+		<ul>
+			
+			<li class="ContCos"><a href="Login">CONT <i class="fa fa-user-circle" ></i></a></li>
+			<li class="ContCos"><a href="ShoppingCart">COȘ <i class="fa fa-shopping-cart"></i></a></li>
+			<li class="ContCos"><a href="MyAccount">WISHLIST <i class="fa fa-heart"></i></a></li>
+		</ul>
+	</nav>
+
 <section>
 <div class=produs>
     <img src="<?php echo BASE_URL . $produs["imagine"]?>" style="width:23%; height:23%;">
     <div class="all">
       <div class="titlu-produs">
        <?php echo (strtoupper($produs["nume"]). ' ' . strtoupper($produs["categorie"])); ?>
-      </div>
+	  </div>
+	  <form method="post" action="#">
 	  	<table class="table-produs" >
         <tr>
           <th>Cantitate</th>
@@ -14,10 +93,10 @@
 				</tr>	   
         <tr>
 					<td>
-					  <label class="radio_button"> 30 ml
-              <input type="radio" name="quantity", value="30">
-                <span class="checkmark"></span>
-            </label>
+					  <label class="radio_button"  name="quantity"> 30 ml
+              <input type="radio" name="quantity" value="<?php echo $produs["pret_30"] ?>">
+				<span class="checkmark"></span>
+			</label>
           </td>              
 					<td> 
 					<?php if(intval($produs["stoc_30"]) == 0) {echo '<p style="color:red">Stoc Insuficient </p>';}
@@ -30,10 +109,10 @@
         </tr>
 				<tr>
 				<td>
-					  <label class="radio_button"> 50 ml
-              <input type="radio" name="quantity", value="50">
-                <span class="checkmark"></span>
-            </label>
+					  <label class="radio_button"  name="quantity"> 50 ml
+              <input type="radio" name="quantity" value="<?php echo $produs["pret_50"] ?>">
+				<span class="checkmark"></span>
+			</label>
 					</td>    
 					<td>              
 					<?php if(intval($produs["stoc_50"]) == 0) {echo '<p style="color:red">Stoc Insuficient </p>';}
@@ -46,10 +125,11 @@
         </tr>
 				<tr>
 				<td>
-					  <label class="radio_button"> 100 ml
-              <input type="radio" name="quantity", value="100">
+
+					  <label class="radio_button" name="quantity"> 100 ml
+              <input type="radio" name="quantity" value="<?php echo $produs["pret_100"] ?>">
                 <span class="checkmark"></span>
-            </label>
+			</label>
           </td>                    
 					<td> 
 					 <?php if(intval($produs["stoc_100"]) == 0) {echo '<p style="color:red">Stoc Insuficient </p>';}
@@ -61,10 +141,11 @@
           </td> 
         </tr>
       </table>
-		<div class="buttons">
-		<button><i class="far fa-heart"></i> Adauga in wishlist</button>
-		<button><i class="fa fa-shopping-cart"></i> Adauga in cos</button>
-	</div>
+	 <div class="buttons"> 
+		<button type="submit" value="wishlist" name="wishlist" ><i class="far fa-heart"></i> Adauga in wishlist</button>
+		<button type="submit" value="basket" name="basket"><i class="fa fa-shopping-cart"></i> Adauga in cos</button>
+	</div>  
+		</form>
   </div>
 
 	</div>
@@ -89,7 +170,7 @@
 				<?php echo $produs["descriere"]; ?>
 		</div>
 		</div>
-		<div class = "comentariu" >
+		<div id = "comentariu" >
 		 <h3> Comentarii clienti </h3>
     <div id="comments" >
 			 <?php 
@@ -100,8 +181,19 @@
 
 			?>
 		</div>
-		<button > Arata mai multe comentarii </button>
-    </div>
+		<button class="commbtn"> Arata mai multe comentarii </button>
+	</div>
+	<div class="bg-modal">
+				<div class="modal-contents">
+			
+					<div class="close">+</div>
+			
+					<form action="">
+						<a href="#" class="buttonb">Intra in cont </a>
+					</form>
+			
+				</div>
+			</div>
   </section>
 
 	<?php include 'footer.php'; ?>
