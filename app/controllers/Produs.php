05332125bda54c7b7af05d269a68cdf1c3d82c72
dianagabr <focadianagabriela@yahoo.com>
2019-06-07 00:38:03 +0300

    Update Produs.php

--- app/controllers/Produs.php
+++ app/controllers/Produs.php
@@ -7,10 +7,37 @@ class Produs extends Controller {
 		$model=new  ProdusModel();
 		$template = $this->loadView('produs-view');
 		$info = $model->getDetails('candy prada');
-		$result = $model -> getComments('candy prada');
+		$result = $model -> getFirstComments('candy prada');
 		$template->set('produs', $info) ;
 		$template->set('comments',$result);
+		if(isset($_POST['wishlist'])){
+			if(isset($_SESSION['email'])){
+				$result = $model -> getId($_SESSION['email']);
+				$id = $result['id'];
+				$result = $model ->addToWishlist($id,$info['id']);
+			}
+			else {
+				echo 'Nu esti logat';
+			}
+		}
+		if(isset($_POST['basket'])){
+			if(isset($_SESSION['email'])){
+				$result = $model -> getId($_SESSION['email']);
+				$id = $result['id'];
+				if(isset($_POST['quantity'])){
+					$price = $_POST['quantity'];
+					$result = $model ->addToBasket($info['id'],$id,$price);
+				}
+				else{
+					echo 'Nu ai selectat cantitatea';
+				}
+			}
+			else{
+				echo 'Nu esti logat';
+			}
+		}
 		$template->render();
+
 	}
 }
 ?>
