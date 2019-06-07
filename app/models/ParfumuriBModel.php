c5170616fc3865ed0a83ca6e66ca2436cdd6922d
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-07 19:30:36 +0200

    Create ParfumuriBModel.php

---
+++ app/models/ParfumuriBModel.php
@@ -0,0 +1,30 @@
+<?php
+class ParfumuriBModel extends Model {
+	
+	public function getDetails()
+	{   
+        $query = "select p.imagine, p.nume, s.pret_30 from produse p join stocuri s on s.id_produs=p.id where p.gen='barbati'";
+
+  	$result= $this->queryAll($query);
+	return $result;
+	}
+
+	public function getRowCount()
+	{   
+        $query = "select count(nume) from produse where gen='barbati'";
+        if(isset($_POST["lowerSlider"], $_POST["upperSlider"]) && !empty($_POST["lowerSlider"]) && !empty($_POST["upperSlider"]))
+	{
+		$query .= "
+		 AND pret BETWEEN '".$_POST["lowerSlider"]."' AND '".$_POST["lowerSlider"]."'
+		";
+	}
+
+	echo $query;
+
+  		$result= $this->query($query);
+	return $result;
+	}
+
+	
+}
+?>
