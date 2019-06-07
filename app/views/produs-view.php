e0a640c770746663867e8e115b563ccb2428482a
dianagabr <focadianagabriela@yahoo.com>
2019-06-08 00:24:26 +0300

    Update produs-view.php

--- app/views/produs-view.php
+++ app/views/produs-view.php
@@ -84,17 +84,18 @@
       <div class="titlu-produs">
        <?php echo (strtoupper($produs["nume"]). ' ' . strtoupper($produs["categorie"])); ?>
 	  </div>
-	  <form method="post" action="#">
+	  <form id="basket" method="post" action="#">
+	  <input type="hidden" name="basket" value=""/>
 	  	<table class="table-produs" >
         <tr>
           <th>Cantitate</th>
           <th>Disponibilitate</th> 
-          <th>Pret</th>
+          <th>Pre&#355;</th>
 				</tr>	   
         <tr>
 					<td>
 					  <label class="radio_button"  name="quantity"> 30 ml
-              <input type="radio" name="quantity" value="<?php echo $produs["pret_30"] ?>">
+              <input type="radio" name="quantity" value="30">
 				<span class="checkmark"></span>
 			</label>
           </td>              
@@ -110,7 +111,7 @@
 				<tr>
 				<td>
 					  <label class="radio_button"  name="quantity"> 50 ml
-              <input type="radio" name="quantity" value="<?php echo $produs["pret_50"] ?>">
+              <input type="radio" name="quantity" value="50">
 				<span class="checkmark"></span>
 			</label>
 					</td>    
@@ -127,7 +128,7 @@
 				<td>
 
 					  <label class="radio_button" name="quantity"> 100 ml
-              <input type="radio" name="quantity" value="<?php echo $produs["pret_100"] ?>">
+              <input type="radio" name="quantity" value="100">
                 <span class="checkmark"></span>
 			</label>
           </td>                    
@@ -140,12 +141,18 @@
 					  <?php echo $produs["pret_100"] . ' LEI';?>
           </td> 
         </tr>
-      </table>
+	  </table>
+	  
+	  </form>
+	  <form method="post" action="#" id="wishlist">
+	  <input type="hidden" name="wishlist" value=""/>
+	  </form>
 	 <div class="buttons"> 
-		<button type="submit" value="wishlist" name="wishlist" ><i class="far fa-heart"></i> Adauga in wishlist</button>
-		<button type="submit" value="basket" name="basket"><i class="fa fa-shopping-cart"></i> Adauga in cos</button>
+		<a  id="buttonw" type="submit" value="wishlist" name="wishlist" ><i class="far fa-heart"></i> Adaug&#259; 	&#238;n wishlist</a>
+		<a  id="buttonc" type="submit" value="basket" name="basket"><i class="fa fa-shopping-cart"></i> Adaug&#259; &#238;n co&#351;</a>
+		
 	</div>  
-		</form>
+	
   </div>
 
 	</div>
@@ -153,13 +160,13 @@
 	<div class="compozitie">
 		<h4>Compozitie</h4>
 		  <div class="note">
-				<h6><b>Note de baza: </b> <?php echo $produs["note_baza"] ?> </h6>
+				<h6><b>Note de baz&#259;: </b> <?php echo $produs["note_baza"] ?> </h6>
 			</div>
 			<div class="note">
-			 <h6><b>Note de inima: </b> <?php echo $produs["note_inima"] ?> </h6>
+			 <h6><b>Note de inim&#259;: </b> <?php echo $produs["note_inima"] ?> </h6>
 		 </div>
 		 <div class="note">
-			 <h6><b>Note de varf: </b> <?php echo $produs["note_varf"] ?> </h6>
+			 <h6><b>Note de v&#226;rf: </b> <?php echo $produs["note_varf"] ?> </h6>
 		 </div>
 		 <div class="note">
 			 <h6> <?php echo 'Parfum ' . $produs["esenta"] ; ?></h6>
@@ -170,8 +177,17 @@
 				<?php echo $produs["descriere"]; ?>
 		</div>
 		</div>
+
+		<div id="adaugacomm" >
+			<form method="post" action="#">
+			        <label for="name" >Nume</label>
+                    <input type="text" id="name" name="name" placeholder="Nume">
+			        <label for="comm" >Comentariu</label>
+                    <textarea name="comm" id="" rows="10" class="form-control" placeholder="Descriere"></textarea>
+		    </form>
+		</div>
 		<div id = "comentariu" >
-		 <h3> Comentarii clienti </h3>
+		 <h4> Comentarii clien&#355;i </h4>
     <div id="comments" >
 			 <?php 
 					foreach($comments as $array) {
@@ -181,7 +197,9 @@
 
 			?>
 		</div>
-		<button class="commbtn"> Arata mai multe comentarii </button>
+				<form method="post" action="#">
+			 <button class="commbtn" type="submit" name="comments"> Vezi comentarii </button>
+				</form>
 	</div>
 	<div class="bg-modal">
 				<div class="modal-contents">
@@ -189,11 +207,125 @@
 					<div class="close">+</div>
 			
 					<form action="">
-						<a href="#" class="buttonb">Intra in cont </a>
+						<a href="login" class="button">Intr&#259; &#238;n cont </a>
 					</form>
 			
 				</div>
 			</div>
-  </section>
 
+			<div class="quantity-alert">
+				<div class="modal-contents">
+			
+					<div class="qclose">+</div>
+			
+				 Trebuie s&#259; alegi o cantitate!
+			
+				</div>
+			</div>
+
+			<div class="stoc-alert">
+				<div class="modal-contents">
+			
+					<div class="sclose">+</div>
+			
+				Cantitatea dorit&#259; nu este 	&#238;n stoc!
+			
+				</div>
+			</div>
+
+			<div class="produs-alert">
+				<div class="modal-contents">
+			
+					<div class="pclose">+</div>
+			
+					<form action="">
+						<a href="shoppingcart" class="button"> Vezi co&#351; </a>
+					</form>
+			
+				</div>
+			</div>
+				
+  </section>
+	 <?php 
+        if(!isset($_SESSION["email"])){
+						echo "
+						<script type=\"text/javascript\">
+						document.getElementById('buttonw').addEventListener(\"click\", function() {
+						document.querySelector('.bg-modal').style.display = \"block\";
+						 });
+						 document.querySelector('.close').addEventListener(\"click\", function() {
+						 document.querySelector('.bg-modal').style.display = \"none\";
+						 });	
+						 document.getElementById('buttonc').addEventListener(\"click\", function() {
+							document.querySelector('.bg-modal').style.display = \"block\";
+							});
+							document.querySelector('.close').addEventListener(\"click\", function() {
+							document.querySelector('.bg-modal').style.display = \"none\";
+							});	
+				 </script>
+						   
+						";
+				   }
+	    else{
+			echo "
+						<script type=\"text/javascript\">
+						document.getElementById('buttonw').addEventListener(\"click\", function() {
+						document.getElementById('wishlist').submit();
+						});
+						document.getElementById('buttonc').addEventListener(\"click\", function() {
+							document.getElementById('basket').submit();
+							});
+				 </script>
+						   
+				";
+		}
+		if(isset($cantitate)){
+		if($cantitate==false){
+				echo "
+				
+				<script type=\"text/javascript\">
+				
+				 document.querySelector('.quantity-alert').style.display = \"block\";
+				 document.querySelector('.qclose').addEventListener(\"click\", function() {
+				 document.querySelector('.quantity-alert').style.display = \"none\";
+				 });	
+		 </script>
+				
+				";
+		}
+	}
+
+	if(isset($stoc)){
+		if($stoc==false){
+				echo "
+				
+				<script type=\"text/javascript\">
+				
+				 document.querySelector('.stoc-alert').style.display = \"block\";
+				 document.querySelector('.sclose').addEventListener(\"click\", function() {
+				 document.querySelector('.stoc-alert').style.display = \"none\";
+				 });	
+		 </script>
+				
+				";
+		}
+	}
+
+	if(isset($success)){
+		if($success==true){
+				echo "
+				
+				<script type=\"text/javascript\">
+				
+				 document.querySelector('.produs-alert').style.display = \"block\";
+				 document.querySelector('.pclose').addEventListener(\"click\", function() {
+				 document.querySelector('.produs-alert').style.display = \"none\";
+				 });	
+		 </script>
+				
+				";
+		}
+	}
+		 
+	 ?>
 	<?php include 'footer.php'; ?>
