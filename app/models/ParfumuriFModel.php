a7d79c04f3a4beafad7645553cdf3f1af7d3e665
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-08 08:37:50 +0200

    Update ParfumuriFModel.php

--- app/models/ParfumuriFModel.php
+++ app/models/ParfumuriFModel.php
@@ -5,9 +5,24 @@ class ParfumuriFModel extends Model {
 	{   
         $query = "select p.imagine, p.nume, s.pret_30 from produse p join stocuri s on s.id_produs=p.id where p.gen='femeie'";
 
-  	$result= $this->queryAll($query);
-	return $result;
+		$result= $this->queryAll($query);
+		return $result;
 	}
+
+	public function getDetailsAsc(){
+		$query= "select p.imagine, p.nume, s.pret_30 from produse p join stocuri s on s.id_produs=p.id where p.gen='femeie' order by s.pret_30 asc";
+		$result= $this->queryAll($query);
+		return $result;
+	}
+
+	public function getDetailsDesc(){
+		$query= "select p.imagine, p.nume, s.pret_30 from produse p join stocuri s on s.id_produs=p.id where p.gen='femeie' order by s.pret_30 desc";
+		$result= $this->queryAll($query);
+		return $result;
+	}
+
+
+	
 
 	public function getRowCount()
 	{   
