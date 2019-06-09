a8b06ae5a5e0f04db45be5a1df2b67f5f9580079
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-09 13:49:28 +0200

    Update RegisterModel.php

--- app/models/RegisterModel.php
+++ app/models/RegisterModel.php
@@ -11,7 +11,7 @@ class RegisterModel extends Model {
 	public function createAccount($nume, $prenume,$adresa, $sex,$judet,$oras,$cod, $email, $telefon, $parola1)
 	{
 
-		$query="insert into utilizatori (nume, prenume, sex, adresa,oras,judet,cod_postal, email,parola, telefon) values ('$nume', '$prenume','$sex','$adresa','$oras','judet','cod', '$email', '$parola1', '$telefon') ";
+		$query="insert into utilizatori (nume, prenume, sex, adresa,oras,judet,cod_postal, email,parola, telefon) values ('$nume', '$prenume','$sex','$adresa','$oras','$judet','$cod', '$email', '$parola1', '$telefon') ";
 
 		$result=$this->queryInsert($query);
 
