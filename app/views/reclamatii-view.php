05c23ba22650aec480d495fa929d69a7438bb77b
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-10 01:40:20 +0200

    Create reclamatii-view.php

---
+++ app/views/reclamatii-view.php
@@ -0,0 +1,32 @@
+<?php 
+include "header.php";
+
+?>
+
+    
+<section >
+<h2 class="title"> Reclamații și plângeri </h2>
+<div >
+
+<br><br><p>
+Garanţii si Reclamaţii<br><p>
+Deşi numărul de reclamaţii este mic, este bine de ştiut că la noi aveţi aceleaşi garanţii ca într-un magazin fizic. Este în interesul nostru să abordăm toate reclamaţiile cât mai repede posibil, astfel încat nu vă faceţi griji că veţi aştepta o procesare mai mult decât este necesar (3-4 săptămâni, termenul legal fiind de 30 de zile lucrătoare).
+<br><p>
+Garanţia începe în ziua în care consumatorul a acceptat produsul. Produsele noastre au 24 de luni garanţie, la fel ca în magazinul nostru. Vânzătorul este responsabil pentru toate defectele produsului livrat către cumpărător care apar în perioada de garantie.
+<br><br><p>
+Solicitare retur
+În cazul în care ați achiziționat produse de la noi și doriți să depuneți o plângere, vă rugăm să luați legătura cu departamentul suport clienți, prin e-mail  info@notino.ro, sau la telefon 031 22 94 544.
+<br><p>
+Cererea de retur va fi înregistrată de noi, astfel încat dumneavoastră să nu fiți taxați pentru acest serviciu.
+Procedură retur
+Atașați produslui o scrisoare de inteție în care se specifică motivul returnării. Anexați o copie a facturii și informații prin care putem identifica achiziția dumneavoastră. Sigilați produsul/produsele într-o cutie de carton astfel încât să nu se deterioreze pe parcursul traseului.
+<br><p>
+Mai mult, vă rugăm să indicați modul în care vom proceda în cazul în care returul dumneavoastră este acceptat, și anume în cazul în care bunurile necesită un schimb (același produs) sau se restituie suma. În cazul unei cereri de restituire a banilor, indicați contul IBAN în RON (24 de caractere) în care doriți să efectuăm restituirea sumei. Nu uitați să ne furnizați detalii complete de contact, numărul comenzii, adresa de e-mail și numărul de telefon, pentru a procesa solicitarea corect și rapid.
+<br><p>
+Produsele nu se pot restitui direct agenților de livrare, în cazul unei reclamații.
+
+
+</div>
+</section>
+
+<?php  include 'footer.php'   ?>
