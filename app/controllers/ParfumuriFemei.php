968e6fee815444cc9a9e586fe512b13457a3d778
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-10 00:58:32 +0200

    Update ParfumuriFemei.php

--- app/controllers/ParfumuriFemei.php
+++ app/controllers/ParfumuriFemei.php
@@ -11,8 +11,12 @@ class ParfumuriFemei extends Controller {
 		$info=$model->getDetails();
 		$info2=$model->getRowCount();
 		$count= (int)$info2[0];
+
+		$info4=$model->getRowVandut();
+		$countVandut=(int)$info4[0];
 	
 		$template->set('total_row',$count);
+		$template->set('total_rowVandut',$countVandut);
 		$template->set('produs', $info);
 		
 
@@ -27,6 +31,22 @@ class ParfumuriFemei extends Controller {
 				{
 					$info3=$model->getDetailsDesc();
 					$template->set('produsDesc', $info3);
+				}
+			}
+			
+		}
+
+		if(isset($_POST["filtru2"])){
+			if(isset($_POST["radio"])){
+				if(($selectRadio= $_POST["radio"]) =='celeMaiNoi'){
+
+					$info3=$model->getDetailsNoi();
+					$template->set('produseNoi', $info3);
+				}
+				else 
+				{
+					$info3=$model->getDetailsVandut();
+					$template->set('produsVandut', $info3);
 				}
 			}
 			
