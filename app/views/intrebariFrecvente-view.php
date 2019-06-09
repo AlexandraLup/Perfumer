3fc948cd3d5a695a325e0488692337f2dacb7a88
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-10 01:40:22 +0200

    Create intrebariFrecvente-view.php

---
+++ app/views/intrebariFrecvente-view.php
@@ -0,0 +1,58 @@
+<?php 
+include "header.php";
+
+?>
+
+    
+<section >
+<h2 class="title"> Întrebări frecvente</h2>
+<div >
+
+<br><br><p>
+
+	1. Ce este parfumul, Colonia sau Apa de toaleta?
+Parfumul are în compoziţe între 15 - 30% uleiuri esentiale; Apa de Parfum (Eau de Parfum) are in compoziţie între 8 - 15% uleiuri esenţiale, iar Apa de Toaleta (Eau de Toilette) are in compoziţie între 4 - 8% uleiuri esentiale. Apa de Colonie (Eau de Cologne) conţine între 3 la 5% uleiuri esenţiale în compoziţie diluate şi în general are 70% alcool.
+
+<br><br><p>
+2. Cum ne putem alege parfumul perfect sau un produs similar?
+Fiecare dintre noi are propriile gusturi. Nu vă orientaţi niciodată după mirosul probelor/testelor de parfum din interiorul flaconului. Aplicaţi parfum sau alte produse similare, pe un tampon de bumbac sau pe orice altă suprafaţă după care lasaţi-o într-un spaţiu deschis, în contact direct cu aerul. Acest tip de probă va reda cu o mai mare fidelitate mirosul parfumului respectiv.
+
+<br><br><p>
+3. Este adevarat că mirosul aceluiaşi parfum este oarecum diferit dacă se aplică la utilizatori diferiţi?
+Da, este adevărat. Orice persoană are mirosul său specific. Depinde de proprietăţile pielii sale, de regimul alimentar si de obiceiurile de viaţă. Parfumul aplicat pe pielea unei persoane poate genera un miros foarte plăcut, însă pe pielea unei alte personae poate cauza un miros putin diferit.
+
+<br><br><p>
+4. Parfumul se modifica daca persoana a consumat alimente picante?
+Parfumul nu se schimbă, dar se modifică mirosul parfumului pe piele, corpul eliberând în procesul de ardere al alimentelor diferite mirosuri în funcţie de alimentele consumate. Acest proces este motivul principal pentru care mirosul unui parfum este peceput în moduri diferite faţă de nota sa generală.
+
+<br><br><p>
+5. Parfumul barbatesc poate afecta starea de spririt?
+Da, foarte mult. Aroma acţionează direct asupra echilibrului psihologic. Esenţele plăcute pot îmbunătăţi imediat starea de spirit şi în opoziţie esenţele ascuţite şi foarte intense pot influenţa negativ starea unei persoane.
+
+<br><br><p>
+6. Exista parfumuri afrodisiace?
+În principiu, nu există, cel puţin nu care actionează în mod direct ca un afrodisiac. Dar, mirosul plăcut, al celui care utilizează parfumul poate atrage interesul şi poate fi provocator. Cand mirosul este mai proaspat sau natural, asa-numitele efecte ,,iritante'' sunt mai mici.
+
+<br><br><p>
+7. Exista reguli speciale de aplicare a unui parfum?
+Da. Parfumul nu ar trebui niciodata sa “terorizeze” împrejurimile. Aici mai mult decât oriunde altundeva se aplica principiul că mai putin este întotdeauna mai mult. În timpul zilei este de dorit sa se folosească Apă de Toaleta sau Apă de Colonie. Parfumul în sine poate fi utilizat numai pentru ocazii speciale. Este necesar să se aleagă un parfum în functie de sezoane. Pentru zilele calde de vară pot fi alese parfumuri cu miresme proaspete, florale sau de citrice si variaţiile acestora. În anotimpurile reci se pot aplica parfumuri de tip oriental, însă aceasta regula nu reprezinta o obligativitate. Alegerea fiecarei persoane depinde de gust si mod de viaţă.
+
+<br><br><p>
+8. De ce nu pot utiliza simultan mai multe arome diferite?
+Fiecare parfum este un ansamblu armonios. În cazul în care în acelasi timp sunt utilizate produse diferite şi distincte aromate intens, se produce o stare de disconfort, care poate produce o impresie neplacută.
+
+<br><br><p>
+9. Cum si unde sa "utilizăm" parfumul?
+Aceste doua aspecte ţin de alegerea noastră personala însă în general, parfumul se aplica la locurile în care pielea este puternic vascularizata, locuri unde pielea este mai caldă, unde mirosul de parfum se dezvolta pe deplin. Parfumul este, de asemenea, aplicat pe par, chiar deasupra capului. Parfumul nu este recomandat a se aplica pe haine din materiale naturale si blănuri, haine de culoare albă sau articole de îmbracaminte din material subţire, mai ales mătase naturală.
+
+<br><br><p>
+10. Cat timp este recomandat a utiliza un parfum si cum ne dam seama ca acesta s-a degradat?
+În cazul în care păstraţi un recipient de parfum cu o expunerea la lumina redusa si temperaturi mai mici de 20 °C, parfumul poate dura caţiva ani. Parfumurile noi conţînand inscripţii care menţionează termenul de valabilitate nu îşi schimbă proprietaţile dupa deschidere. Termenul de valabilitate este diferit în funcţie de miros şi variază între 12- 36 de luni. Parfumul menţinut în flaconul original nedesfăcut se va menţine caţiva ani. Un parfum care şi-a modificat culoarea, într-o nuanţă mai închisă mai ales in jurul inelului capacului este, de obicei vechi. De asemenea, mirosurile acide şi răşinoase indica faptul că parfumul sau produsele similare au deja o durată de depozitare mare.
+
+
+
+
+</div>
+</section>
+
+<?php  include 'footer.php'   ?>
