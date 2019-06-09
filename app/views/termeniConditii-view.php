e31913ea3cc1124dc8129b769ca7f6de978955f9
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-10 01:40:16 +0200

    Create termeniConditii-view.php

---
+++ app/views/termeniConditii-view.php
@@ -0,0 +1,66 @@
+<?php 
+include "header.php";
+
+?>
+
+    
+<section >
+<h2 class="title"> Termeni și condiții </h2>
+<div >
+
+
+<p>1. Prevederi generale
+
+<br>
+<p>
+Termenii și condițiile se aplică la achiziționarea online, de pe site-ul www.perfumer.ro, stabilesc condițiile și specifică drepturile și obligațiile vânzătorului (distribuitorului) și ale cumpărătorului (clientului, consumatorului). Vânzător și operator al operațiunilor online este Perfumer, s.r.o., cu sediul în Brno, Londýnské náměstí 881/6, Brno, 63900, cod fiscal: 27609057. Compania este înregistrată la Tribunalul regional din Brno în documentul cu numărul C 101025. Pentru informațiile de contact vă rugăm să consultați secțiunea contact de pe site (această secțiune este parte din termenii și condițiile prezente).
+Relația dintre părți este guvernata de următoarele acte normative:
+<br>
+<p>
+a.OG nr. 21/1992 privind protecţia consumatorilor
+<p>
+b.OUG nr. 34/2014 privind drepturile consumatorilor în cadrul contractelor încheiate cu profesioniștii
+<p>
+c.Legea nr. 363/2007 privind combaterea practicilor incorecte ale comercianţilor în relația cu consumatorii și armonizarea reglementărilor cu legislația europeană privind protecția consumatorilor
+<p>
+d.Legea 365/2002 privind comerțul electronic
+
+<br><br>
+<p>
+2. Comanda
+<br>
+<p>
+Cumpărătorul poate efectua comenzi prin adăugarea produsului/produselor în coșul de cumpărături urmând apoi pașii în care va selecta modalitatea de plată și va introduce datele de contact și de livrare. Adăugarea unui produs în coșul de cumpărături în lipsa finalizării comenzii nu atrage după sine înregistratrarea unei comenzi și implicit nici rezervarea automată a produsului.
+<p>
+După finalizare, detaliile comenzii se pot verifica atât în atașamentul mesajului de confirmare cât și în secțiunea „istoric comenzi” din contul de utilizator de pe site-ul nostru.
+
+Prin finalizarea comenzii cumpăratorul agrează că toate datele furnizate, necesare procesului de cumpărare, sunt corecte, complete și adevărate.
+
+<br><br>
+<p>3. Definiții
+  <br>
+  <p>
+Contract de vânzare - orice contract în temeiul căruia vânzătorul transferă sau se angajează să transfere proprietatea asupra unor produse către consumator, iar consumatorul plătește sau se angajează să plătească prețul acestora.
+<p>Vânzătorul (distribuitorul) - o persoană, care în concordanță cu termenii și condițiile contractuale oferă produsul  consumatorului. Este orice persoană fizică sau juridică, publică sau privată, care acționează în cadrul activității sale comerciale, industriale sau de producție, artizanale ori liberale în legătura cu contractele care intră sub incidenta OUG nr. 34/2014, precum și orice persoană care acționează în același scop, în numele său pe seama acesteia.
+ <p>Consumator (cumpărătorul) - orice persoană fizică sau grup de persoane fizice constituite în asociații, care în concordanță cu termenii și condițiile contractuale, acţionează în scopuri din afara activității sale comerciale, industriale sau de producție, artizanale ori liberale.
+ <p>Distribuitor (operatorul economic din lanțul de distribuție) - o persoană fizică sau juridică, autorizată, care, în cadrul activității sale profesionale, în concordanță cu termenii și condițiile contractuale, fabrică, importă, transportă sau comercializează produse ori părți din acestea sau prestează servicii.
+ <p>Încheierea contractului de vânzare - comanda facută de cumpărător este un act cu efecte juridice, sub condiția confirmării și acceptării comenzii de către vânzător. Din momentul acceptării comenzii de către vânzător  și recepționării acceptării comenzii de către cumpărător între părți este încheiat un contract generator de drepturi și obligații în sarcina ambelor părti, asa cum sunt definite ele în contract sau în termenii și condițiile prezente, care sunt parte integrantă din acest contract.
+ <p>Termeni și condiții - înțelegerea contractuală dintre vânzător și cumpărător, în conformitate cu termenii și condițiile arătate în această secțiune. Cumpărătorul acceptă termenii în care este încheiat contractul la momentul transmiterii comenzii, comanda care generează efecte juridice de la momentul încheierii contractului de vânzare prin confirmarea și acceptarea comenzii de către vânzător. Cumpărătorul se obligă sa ia la cunoștință prezentele condiții, condiții pe care se consideră ca le-a acceptat din momentul transmiterii comenzii către vânzător.
+
+  <br><br>
+  <p>4. Procesarea comenzilor<br>
+După înregistrarea comenzii, veti primi prin e-mail un răspuns automat de confirmare. 
+<p>Comenziile primite până în ora 12, sunt de regula procesate în aceeași zi sau în urmatoarea zi lucrătoare. Produsele sunt de regulă livrate în termen de 3-4 zile lucrătoare. Cu toate ca 98% din produse sunt tot timpul în stoc, în mod excepțional, termenul de livrare se poate prelungi (datorită volumului ridicat de muncă).
+
+ <br><br>
+5. Livrare <br><p>
+Taxa de transport este de 15 RON (respectiv 6 RON) la comezile de sub 250 RON, sau compania de curierat selectată. Costul de expediere pe care cumpărătorul îl alege în comanda supărtată de către acesta este guvernată de prețul curent al vânzătorului care se află pe pagina de: Cum pot plăti? 
+<p>
+Livrarea coletelor este posibilă numai pe teritoriul României.
+
+
+
+</div>
+</section>
+
+<?php  include 'footer.php'   ?>
