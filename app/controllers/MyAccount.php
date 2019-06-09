612d5d1f9080319cd4fda556de7e37f7814c9c0d
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-10 00:58:35 +0200

    Update MyAccount.php

--- app/controllers/MyAccount.php
+++ app/controllers/MyAccount.php
@@ -1,10 +1,25 @@
 <?php
+include APP_DIR . 'models/MyAccountModel.php';
 class MyAccount extends Controller {
 	
 	function index()
 	{
-	   
+	   	$model= new MyAccountModel();
 		$template = $this->loadView('myAccount-view');
+
+		if(isset($_SESSION["id"])){
+			$info = $model->getDetailsWish($_SESSION["id"]);
+			$info2= $model->getRowWish($_SESSION["id"]);
+			$count= (int)$info2[0];
+
+			$template->set('produsWish', $info);
+			$template->set('rowWish', $count);
+
+		}else{
+			$count= 0;
+			$template->set('rowWish', $count);
+		}
+
 		$template->render();
 	}
 
