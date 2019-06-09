42b03310a062b6d395c88d85321436abb842cd1d
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-10 01:40:28 +0200

    Create despreNoi-view.php

---
+++ app/views/despreNoi-view.php
@@ -0,0 +1,52 @@
+<?php 
+include "header.php";
+
+?>
+
+    
+<section >
+<h2 class="title"> Despre noi </h2>
+<div >
+
+
+<p>DE CE PERFUMER?
+ 
+„Noi, cei de la Perfumer credem că frumusețea ar trebui să fie accesibilă oricui, pentru că aduce încredere și schimbări pozitive în viața fiecăruia. Oamenii care se simt frumoși își ating mai ușor obiectivele și, la rândul lor, fac lumea din jur mai frumoasă.“
+
+<br><br><br><br><br>
+
+<p>DE CE PERFUMER?
+ 
+„Noi, cei de la Perfumer credem că frumusețea ar trebui să fie accesibilă oricui, pentru că aduce încredere și schimbări pozitive în viața fiecăruia. Oamenii care se simt frumoși își ating mai ușor obiectivele și, la rândul lor, fac lumea din jur mai frumoasă.“
+
+
+
+<br><br><br><br><br>
+
+<p>DE CE PERFUMER?
+ 
+„Noi, cei de la Perfumer credem că frumusețea ar trebui să fie accesibilă oricui, pentru că aduce încredere și schimbări pozitive în viața fiecăruia. Oamenii care se simt frumoși își ating mai ușor obiectivele și, la rândul lor, fac lumea din jur mai frumoasă.“
+
+
+
+<br><br><br><br><br>
+
+<p>DE CE PERFUMER?
+ 
+„Noi, cei de la Perfumer credem că frumusețea ar trebui să fie accesibilă oricui, pentru că aduce încredere și schimbări pozitive în viața fiecăruia. Oamenii care se simt frumoși își ating mai ușor obiectivele și, la rândul lor, fac lumea din jur mai frumoasă.“
+
+
+
+<br><br><br><br><br>
+
+<p>DE CE PERFUMER?
+ 
+„Noi, cei de la Perfumer credem că frumusețea ar trebui să fie accesibilă oricui, pentru că aduce încredere și schimbări pozitive în viața fiecăruia. Oamenii care se simt frumoși își ating mai ușor obiectivele și, la rândul lor, fac lumea din jur mai frumoasă.“
+
+
+
+
+</div>
+</section>
+
+<?php  include 'footer.php'   ?>
