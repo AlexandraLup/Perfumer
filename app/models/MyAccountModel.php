e3ad9b3ac4d1a0dc245786fca0bca2aa8ad493b2
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-10 00:58:21 +0200

    Update MyAccountModel.php

--- app/models/MyAccountModel.php
+++ app/models/MyAccountModel.php
@@ -27,6 +27,26 @@ class MyAccountModel extends Model{
 
 
 
+
+   public function getDetailsWish($id)
+  {   
+        $query = "select p.imagine, p.nume, s.pret_30 from produse p join stocuri s on s.id_produs=p.id join wishlist w on w.id_produs=p.id where w.id_utilizator='$id'";
+
+    $result= $this->queryAll($query);
+    return $result;
+  }
+
+   public function getRowWish($id)
+  {   
+        $query = "select count(p.id) from produse p join stocuri s on s.id_produs=p.id join wishlist w on w.id_produs=p.id where w.id_utilizator='$id'";
+
+    $result= $this->query($query);
+    return $result;
+  }
+
+
+
+
 }
 
 
