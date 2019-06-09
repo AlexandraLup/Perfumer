e8683e4b7e26b4271a4d93efe091a114a7bec709
dianagabr <focadianagabriela@yahoo.com>
2019-06-09 13:55:30 +0300

    Update MyAccount.php

--- app/controllers/MyAccount.php
+++ app/controllers/MyAccount.php
@@ -7,5 +7,16 @@ class MyAccount extends Controller {
 		$template = $this->loadView('myAccount-view');
 		$template->render();
 	}
+
+	function settings(){
+		
+		$template = $this->loadView('personaldata-view');
+		$model=$this->loadModel('MyAccountModel');
+		$array=$model->getId($_SESSION['email']);
+		$userID = $array['id'];
+		$userDetails = $model->getPersonalData($userID);
+	    $template->set('details',$userDetails);
+		$template->render();
+	}
 }
 ?>
