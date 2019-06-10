9a5c71ac3ea61dad26b6740f77a6034ab7801b81
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-10 19:06:14 +0200

    Update Noutati.php

--- app/controllers/Noutati.php
+++ app/controllers/Noutati.php
@@ -10,10 +10,29 @@ class Noutati extends Controller {
 
 		$info=$model->getDetails();
 		$info2=$model->getRowCount();
-		$count= (int)20;
+		$count= (int)15;
 	
 		$template->set('total_row',$count);
 		$template->set('produs', $info);
+
+
+
+		if(isset($_POST["filtru"])){
+			if(isset($_POST["radio"])){
+				if(($selectRadio= $_POST["radio"]) =='asc'){
+
+					$info3=$model->getDetailsAsc();
+					$template->set('femei', $info3);
+				}
+				else 
+				{
+					$info3=$model->getDetailsDesc();
+					$template->set('barbati', $info3);
+				}
+			}
+			
+		}
+
 		
 		$template->render();
 	}
