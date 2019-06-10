341c66258632da2cc165d94d0fe27c021af0e345
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-10 19:06:17 +0200

    Update ParfumuriBarbati.php

--- app/controllers/ParfumuriBarbati.php
+++ app/controllers/ParfumuriBarbati.php
@@ -3,6 +3,7 @@ include APP_DIR . 'models/ParfumuriBModel.php';
 
 class ParfumuriBarbati extends Controller {
 	
+
 	function index()
 	{
 	    $model= new ParfumuriBModel();
@@ -11,9 +12,47 @@ class ParfumuriBarbati extends Controller {
 		$info=$model->getDetails();
 		$info2=$model->getRowCount();
 		$count= (int)$info2[0];
+
+		$info4=$model->getRowVandut();
+		$countVandut=(int)$info4[0];
 	
 		$template->set('total_row',$count);
+		$template->set('total_rowVandut',$countVandut);
 		$template->set('produs', $info);
+		
+
+		if(isset($_POST["filtru"])){
+			if(isset($_POST["radio"])){
+				if(($selectRadio= $_POST["radio"]) =='asc'){
+
+					$info3=$model->getDetailsAsc();
+					$template->set('produsAsc', $info3);
+				}
+				else 
+				{
+					$info3=$model->getDetailsDesc();
+					$template->set('produsDesc', $info3);
+				}
+			}
+			
+		}
+
+		if(isset($_POST["filtru2"])){
+			if(isset($_POST["radio"])){
+				if(($selectRadio= $_POST["radio"]) =='celeMaiNoi'){
+
+					$info3=$model->getDetailsNoi();
+					$template->set('produseNoi', $info3);
+				}
+				else 
+				{
+					$info3=$model->getDetailsVandut();
+					$template->set('produsVandut', $info3);
+				}
+			}
+			
+		}
+		
 		
 		$template->render();
 	}
