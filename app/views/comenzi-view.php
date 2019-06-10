9f82f747e95d45010f98e81ae8cd7786154d0f26
dianagabr <focadianagabriela@yahoo.com>
2019-06-10 17:50:28 +0300

    Create comenzi-view.php

---
+++ app/views/comenzi-view.php
@@ -0,0 +1,59 @@
+
+<?php   include 'header.php'; ?>
+<section>
+<div class="menu">
+    <div class="lista">
+    <a href="http://localhost/perfumer/MyAccount"><i class="far fa-heart"></i> Produse favorite</a>
+    </div>
+    <div class="lista">
+    <a href="settings"><i class="far fa-clipboard"></i> Date personale</a>
+    </div>
+    <div class="lista">
+    <a href="#"><i class="fas fa-warehouse"></i> Istoricul comenzilor</a>
+    </div>
+</div>
+<div class="title1">
+<h3> Comenzi </h3>
+<p> Vizualizați toate comenzile actuale și precedente aici.</p>
+</div>
+<div class="history">
+	<div class="order">
+  <div class="order-info">
+    <p><b>NUMĂR COMANDĂ 33456125456</b></p>
+  <p class="order-date">2019-05-13</p>
+    </div>
+    <div class="order-status">
+        <p >Expediată</p>
+			</div>
+    </div>
+			<div class="order">
+    <div class="order-info">
+      <p ><b>NUMĂR COMANDĂ 33456125456</b></p>
+    <p class="order-date">2019-05-13</p>
+      </div>
+    <div class="order-status">
+        <p  style="color:green;">Livrată</p>
+    </div>
+</div>
+	<div class="order">
+      <div class="order-info">
+        <p ><b>NUMĂR COMANDĂ 33456125456</b></p>
+      <p class="order-date">2019-05-13</p>
+        </div>
+        <div class="order-status">
+            <p  style="color:green;">Livrată</p>
+        </div>
+			</div>
+				<div class="order">
+        <div class="order-info">
+          <p ><b>NUMĂR COMANDĂ 33456125456</b></p>
+        <p class="order-date">2019-05-13</p>
+          </div>
+          <div class="order-status">
+              <p  style="color:red;">Anulată</p>
+          </div>
+				</div>
+</div>
+</section>
+
+<?php   include 'footer.php'; ?>
