5c6ad1eb48e5dcd8d8ef63b956026aff40455cf9
dianagabr <focadianagabriela@yahoo.com>
2019-06-10 17:50:50 +0300

    Update produs-view.php

--- app/views/produs-view.php
+++ app/views/produs-view.php
@@ -1,82 +1,4 @@
-
-<!DOCTYPE html>
-<html lang="RO">
-
-<head>
-	 <meta charset="UTF-8">
-	<title>Perfumer</title>
-	<link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/styles.css"    type="text/css" />
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">  </script>
-	<script>
-     $(document).ready(function(){
-         var commentCount = 2;
-			 $(".commbtn").click(function(){
-           $("#comentariu").load("load-comments.php");
-			 });
-
-		 });
-	</script>
-</head>
-
-
-<body>
-
-	<header>
-		<div id="Div" >
-			<div id="Div1">
-				<form id="search-button" method="post" action="#">
-					<input type="search" name="search" id="search" placeholder="Caută..">
-				</form>
-				<div id="back_result"> 
-                </div>  
-			</div>
-
-			<div id="Div2">
-				<div>
-					<h1 class="h1"><a href="index.php">Perfumer</a></h1>
-				</div>
-			</div>
-
-			<div id="Div3">
-				<div>
-				<?php 
-					if(isset($_SESSION["email"]))
-
-						echo '<p style="color: white;"> Bună, ' . $_SESSION['email'] .
-					" <form method='post'  >
-					<input type='submit' name='logout' value='logout'>  
-					</form> </p>
-					";
-					else
-						echo '<p style="color: white;"> Încă nu ești logat!</p>';
-				 ?> 
-
-				</div>
-			</div>
-
-		</div>
-
-	</header>
-
-	<nav>
-		<ul>
-			<li class="FemeiBarbati"><a href='parfumurifemei'>PARFUMURI FEMEI</a></li>
-			<li class="FemeiBarbati"><a href='parfumurifemei'>PARFUMURI BĂRBAȚI</a></li>
-			<li class="FemeiBarbati"><a href="parfumurifemei">NOUTĂȚI</a></li>
-		</ul>
-
-		<ul>
-			
-			<li class="ContCos"><a href="Login">CONT <i class="fa fa-user-circle" ></i></a></li>
-			<li class="ContCos"><a href="ShoppingCart">COȘ <i class="fa fa-shopping-cart"></i></a></li>
-			<li class="ContCos"><a href="MyAccount">WISHLIST <i class="fa fa-heart"></i></a></li>
-		</ul>
-	</nav>
-
+<?php      include 'header.php';     ?>
 <section>
 <div class=produs>
     <img src="<?php echo BASE_URL . $produs["imagine"]?>" style="width:23%; height:23%;">
@@ -184,7 +106,7 @@
                     <input type="text" id="name" name="name" placeholder="Nume">
 			        <label for="comm" >Comentariu</label>
 					<textarea name="comm" id="" rows="10" class="form-control" placeholder="Comentariu"></textarea>
-					<button type="submit" value="addcomm" name="addcomm">Adaug&#259; comentariu</button>
+					<button type="submit" value="addcomm" name="addcomm" class="button-model">Adaug&#259; comentariu</button>
 		    </form>
 		</div>
 		<div id = "comentariu" >
