7414eef448011ecbfd0dd7af7a4d275f63434c28
dianagabr <focadianagabriela@yahoo.com>
2019-06-13 00:30:11 +0300

    Update Order.php

--- app/controllers/Order.php
+++ app/controllers/Order.php
@@ -13,8 +13,12 @@ public function search($orderID){
 		$template = $this->loadView('order-view');
 
 		$proVars = str_replace('%20',' ',$orderID);
-        $info = $model->getOrder($userID,$proVars);
-        var_dump($info);
+		$info = $model->getOrder($userID,$proVars);
+		$infouser = $model->getPersonalData($userID);
+		$infoproduct = $model->getOrderProducts($orderID);
+		$template->set('order', $info);
+		$template->set('user', $infouser);
+		$template->set('products', $infoproduct);
 	    $template->render();
 	
 
