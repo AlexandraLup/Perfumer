a9f92a69058bfea7ca3e152e6479c77b515aba64
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-12 12:20:55 +0200

    Update parfumurifemei-view.php

--- app/views/parfumurifemei-view.php
+++ app/views/parfumurifemei-view.php
@@ -61,7 +61,7 @@
             <div class="col-sm-4 col-lg-3 col-md-3">
               <div class="btnn-1">
                 <img src="'. $produsAsc[$i]['imagine'] .'" alt="" class="img-responsive" >
-                <p align="center"><strong><a href="">'. $produsAsc[$i]['nume'] .'</a></strong></p>
+                <p align="center"><strong><a href="http://localhost/perfumer/produs/search/'.$produsAsc[$i]['nume']. ' " >'. $produsAsc[$i]['nume'] .'</a></strong></p>
                 <p style="text-align:center;" class="text-danger" >'.'<strong>De la '. $produsAsc[$i]['pret_30'] .' RON</strong>'.'</h4>
                  </div>
               </div>
@@ -79,7 +79,7 @@
             <div class="col-sm-4 col-lg-3 col-md-3">
               <div class="btnn-1">
                 <img src="'. $produsDesc[$i]['imagine'] .'" alt="" class="img-responsive" >
-                <p align="center"><strong><a href="#">'. $produsDesc[$i]['nume'] .'</a></strong></p>
+                <p align="center"><strong><a href="http://localhost/perfumer/produs/search/'.$produsDesc[$i]['nume']. ' " >'. $produsDesc[$i]['nume'] .'</a></strong></p>
                 <p style="text-align:center;" class="text-danger" >'.'<strong>De la '. $produsDesc[$i]['pret_30'] .' RON</strong>'.'</h4>
                  </div>
               </div>
@@ -97,7 +97,7 @@
             <div class="col-sm-4 col-lg-3 col-md-3">
               <div class="btnn-1">
                 <img src="'. $produseNoi[$i]['imagine'] .'" alt="" class="img-responsive" >
-                <p align="center"><strong><a href="#">'. $produseNoi[$i]['nume'] .'</a></strong></p>
+                <p align="center"><strong><a href="http://localhost/perfumer/produs/search/'.$produseNoi[$i]['nume']. ' " >'. $produseNoi[$i]['nume'] .'</a></strong></p>
                 <p style="text-align:center;" class="text-danger" >'.'<strong>De la '. $produseNoi[$i]['pret_30'] .' RON</strong>'.'</h4>
                  </div>
               </div>
@@ -117,7 +117,7 @@
             <div class="col-sm-4 col-lg-3 col-md-3">
               <div class="btnn-1">
                 <img src="'. $produsVandut[$i]['imagine'] .'" alt="" class="img-responsive" >
-                <p align="center"><strong><a href="#">'. $produsVandut[$i]['nume'] .'</a></strong></p>
+                <p align="center"><strong><a href="http://localhost/perfumer/produs/search/'.$produsVandut[$i]['nume']. ' " >'. $produsVandut[$i]['nume'] .'</a></strong></p>
                 <p style="text-align:center;" class="text-danger" >'.'<strong>De la '. $produsVandut[$i]['pret_30'] .' RON</strong>'.'</h4>
                  </div>
               </div>
@@ -131,13 +131,14 @@
         $output = '';
         if($total_row > 0) {
          for ($i=0; $i<$total_row;$i++) { 
-           
+            
             $output .= '
             <div class="fotoNoutati">
             <div class="col-sm-4 col-lg-3 col-md-3">
               <div class="btnn-1">
+
                 <img src="'. $produs[$i]['imagine'] .'" alt="" class="img-responsive" >
-                <p align="center"><strong><a href="produs/casa='.$produs[$i]['nume'].'">'. $produs[$i]['nume'] .'</a></strong></p>
+                <p align="center"><strong><a href="http://localhost/perfumer/produs/search/'.$produs[$i]['nume']. ' " >'. $produs[$i]['nume'] .'</a></strong></p>
                 <p style="text-align:center;" class="text-danger" >'.'<strong>De la '. $produs[$i]['pret_30'] .' RON</strong>'.'</h4>
                  </div>
               </div>
@@ -157,58 +158,7 @@
 
 </section>
 
-<script>
-$(document).ready(function(){
 
-    filter_data();
-
-    function filter_data()
-    {
-        $('.filter_data').html('<div id="loading" style="" ></div>');
-        var action = 'fetch_data';
-        var minimum_price = $('#lower').val();
-        var maximum_price = $('#upper').val();
-      
-        $.ajax({
-            url:"fetch_data.php",
-            method:"POST",
-            data:{action:action, minimum_price:minimum_price, maximum_price:maximum_price},
-            success:function(data){
-                $('.filter_data').html(data);
-            }
-        });
-    }
-
-    function get_filter(class_name)
-    {
-        var filter = [];
-        $('.'+class_name+':checked').each(function(){
-            filter.push($(this).val());
-        });
-        return filter;
-    }
-
-    $('.common_selector').click(function(){
-        filter_data();
-    });
-
-    $('#price_range').slider({
-        range:true,
-        min:1,
-        max:5000,
-        values:[1, 5000],
-        step:500,
-        stop:function(event, ui)
-        {
-            $('#price_show').html(ui.values[0] + ' - ' + ui.values[1]);
-            $('#hidden_minimum_price').val(ui.values[0]);
-            $('#hidden_maximum_price').val(ui.values[1]);
-            filter_data();
-        }
-    });
-
-});
-</script>
 
 
 
