9007335e7d446f8a8c4b4737e5aea180f0bc672d
dianagabr <focadianagabriela@yahoo.com>
2019-06-13 01:27:46 +0300

    Update ProdusModel.php

--- app/models/ProdusModel.php
+++ app/models/ProdusModel.php
@@ -3,7 +3,7 @@ class ProdusModel extends Model {
 	
 	public function getDetails($name)
 	{   
-        $query = 'SELECT p.id,p.nume, p.gen,p.categorie,p.esenta,p.descriere,p.imagine,s.stoc_30,s.pret_30,s.stoc_50,s.pret_50, s.stoc_100,s.pret_100 , c.note_varf, c.note_inima, c.note_baza FROM `produse` p inner join stocuri s on p.id=s.id_produs  join compozitii c on c.id_produs= p.id WHERE nume="'. $name .'"';
+        $query = 'SELECT p.id,p.nume, p.gen,p.categorie,p.esenta,p.descriere,p.imagine,s.stoc_30,s.pret_30,s.stoc_50,s.pret_50, s.stoc_100,s.pret_100 , c.note_varf, c.note_inima, c.note_baza FROM `produse` p inner join stocuri s on p.id=s.id_produs  join compozitii c on c.id_produs= p.id WHERE nume ="'. $name .'"';
 		$result = $this->query($query);
 		return $result;
 	}
