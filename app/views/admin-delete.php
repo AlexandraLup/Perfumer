21efe632fa270a19c9b419518c20766f891439de
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-13 00:13:30 +0200

    Update admin-delete.php

--- app/views/admin-delete.php
+++ app/views/admin-delete.php
@@ -22,7 +22,7 @@
 		</div>
     </header>
     <div class="navbar">
-  <a \href="rapoarte"><i class="fas fa-file"></i> Rapoarte</a> 
+  <a href="rapoarteVanzari"><i class="fas fa-file"></i> Rapoarte</a> 
   <a href="add"><i class="fas fa-plus"></i> Adaug&#259; produs</a> 
   <a class="active" href="#"><i class="fas fa-trash-alt"></i> &#350;terge produs</a> 
 </div>
