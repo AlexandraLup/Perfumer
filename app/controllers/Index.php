3c4cfa6f22498a96a5465c51b51c80c52726af0b
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-12 15:18:25 +0200

    Update Index.php

--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -6,39 +6,38 @@ class Index extends Controller {
 	{
 	    $model=new IndexModel();
 		$template = $this->loadView('index-view');
-        
+
+        //preferinte
+        if(isset($_SESSION["id"])){
+        	$template->set('afiseazaPreferinte','ok');
+        	  
+    		$infoPp=$model->getEsente($_SESSION["id"]);
+    		$infoP= $model->getDetailsP($infoPp[0]['esenta'],$infoPp[1]['esenta']);
+			$count= $model->getTotalRow($infoPp[0]['esenta'],$infoPp[1]['esenta']);
+			
+			$template->set('produsP', $infoP);
+			
+			$count2=(int)5;
+			if((int)$count[0] <= 5){
+				$template->set('total_rowP',(int)$count[0]);
+			}
+			else{
+				$template->set('total_rowP',$count2);}
+        }
+       
+
+
+        //top noutati
         $info=$model->getDetails();
-		$info2=$model->getRowCount();
 		$count= (int)4;
-	
 		$template->set('total_row',$count);
 		$template->set('produs', $info);
 
-
-
-		if(isset($_POST["filtru"])){
-			if(isset($_POST["radio"])){
-				if(($selectRadio= $_POST["radio"]) =='asc'){
-
-					$info3=$model->getDetailsAsc();
-					$template->set('femei', $info3);
-				}
-				else 
-				{
-					$info3=$model->getDetailsDesc();
-					$template->set('barbati', $info3);
-				}
-			}
-			
-		}
-
-		
-	
  		
+
+
 		$template->render();
-			
-			
-		
+				
 	}
 }
 ?>
