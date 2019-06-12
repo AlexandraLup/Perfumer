0237a69836de5d7c5751fcfb7abd90ea82acd001
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-06-12 12:20:32 +0200

    Update Index.php

--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -7,10 +7,10 @@ class Index extends Controller {
 	   //	$model=new Login_model();
 		$template = $this->loadView('index-view');
         
- 		if(isset($_POST["logout"])){
+ 		/*if(isset($_POST["logout"])){
 			session_destroy();
 			$this->redirect('login');
-		}
+		}*/
 		$template->render();
 			
 			
