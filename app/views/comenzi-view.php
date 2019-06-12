363ea0e1ece435223c9c5322c78111451b81d8ae
dianagabr <focadianagabriela@yahoo.com>
2019-06-12 23:51:38 +0300

    Update comenzi-view.php

--- app/views/comenzi-view.php
+++ app/views/comenzi-view.php
@@ -20,6 +20,7 @@
 <div class="history">
   <?php 
         foreach($details as $array){
+            echo '<a href="http://localhost/perfumer/order/search/'.$array['id']. ' " >' ;
              echo '	<div class="order">';
              echo ' <div class="order-info">';
              echo ' <p style="width:100%; padding:5px;"><b>NUMĂR COMANDĂ : ' . $array['id'] . '</b></p>';
@@ -28,11 +29,14 @@
              echo'<div class="order-status" style="padding:10px;">';
              if(strcmp($array['status'],'expediata')==0){
                  echo '<p  ><b>Expediată</b></p>';
+             }else if(strcmp($array['status'],'livrata')==0){
+                 echo ' <p style="color:green;"><b>Livrată</b></p>';
              }else{
-                 echo ' <p  style="color:green;"><b>Livrată</b></p>';
+                echo ' <p ><b>In curs de procesare</b></p>';
              }
              echo '</div>';
              echo '</div>';
+             echo '</a>';
         }
   ?>
 
