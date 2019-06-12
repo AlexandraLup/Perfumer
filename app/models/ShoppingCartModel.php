2e7280840bc1c2afcf5b723d9a281f530427aaa6
dianagabr <focadianagabriela@yahoo.com>
2019-06-12 13:52:37 +0300

    Update ShoppingCartModel.php

--- app/models/ShoppingCartModel.php
+++ app/models/ShoppingCartModel.php
@@ -7,5 +7,29 @@ class ShoppingCartModel extends Model {
         $result= $this->queryAll($query);
         return $result;
      }
+
+   public function getProductPrice($idProdus, $ml){
+      $query = "SELECT pret_30,pret_50,pret_100  FROM  stocuri  WHERE id_produs='$idProdus'";
+      $result = $this->query($query);
+      $cantitate='pret_' . $ml;
+      return $result[$cantitate];
+   }
+
+   public function updateQTY($idUser,$idProdus, $quantity,$ml){
+      $query= "UPDATE cos_cumparaturi SET cantitate ='$quantity' where id_utilizator='$idUser' and id_produs='$idProdus'";
+      $pret = intval($this->getProductPrice($idProdus, $ml));
+      $subtotal = intval($quantity) * $pret;
+      $query1 =" UPDATE `cos_cumparaturi` SET `subtotal`= '$subtotal' where id_produs='$idProdus' and id_utilizator='$idUser'";
+		$result = $this->execute($query);
+		$result = $this->execute($query1);
+		return $result;
+   }
+
+   public function verifyStoc($idProdus, $ml){
+      $query = "SELECT stoc_30,stoc_50,stoc_100  FROM  stocuri  WHERE id_produs='$idProdus'";
+      $result = $this->query($query);
+      $cantitate='stoc_' . $ml;
+      return $result[$cantitate];
+   }
 }
 ?>
