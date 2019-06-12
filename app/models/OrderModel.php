aafe097e29c6b1e79d391b777d0bb17cb704e3e2
dianagabr <focadianagabriela@yahoo.com>
2019-06-13 00:30:14 +0300

    Update OrderModel.php

--- app/models/OrderModel.php
+++ app/models/OrderModel.php
@@ -6,5 +6,19 @@ class OrderModel extends Model {
         $result= $this->query($query);
         return $result;
       }
+
+      
+    public function getPersonalData($idUser){
+            
+      $query= "select nume,prenume,sex,adresa,email,telefon,oras,judet,cod_postal from utilizatori where id='$idUser'";
+      $result= $this->query($query);
+      return $result;
+   }
+
+   public function getOrderProducts($idOrder){
+    $query="select l.subtotal, l.cantitate, l.marime, p.imagine, p.nume, p.categorie from lista_produse l inner join produse p on p.id=l.id_produs where l.id_comanda='$idOrder'";
+    $result= $this->queryAll($query);
+      return $result;
+   }
 }
 ?>
